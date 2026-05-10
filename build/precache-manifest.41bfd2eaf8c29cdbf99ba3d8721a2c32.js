d261f3d819a199b5a9fee8ab46877e9bea01b780
David Loynaz <dloynaz@hotmail.com>
2026-05-10 14:07:09 +0200

    Add SEO/OG meta tags and improve portfolio link integration

---
+++ build/precache-manifest.41bfd2eaf8c29cdbf99ba3d8721a2c32.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82a4e0a903c29c1d5fdac42c6553bc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5844d453f851aee019f",
+    "url": "/static/css/main.796d04ea.chunk.css"
+  },
+  {
+    "revision": "790106af30f4f5975f06",
+    "url": "/static/js/2.2df919bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2df919bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5844d453f851aee019f",
+    "url": "/static/js/main.af4393b1.chunk.js"
+  },
+  {
+    "revision": "4569d4d938faf031954c",
+    "url": "/static/js/runtime-main.6109b15a.js"
+  },
+  {
+    "revision": "fee734fa8e9a6adbca735d68fce75297",
+    "url": "/static/media/CupTexture.fee734fa.webm"
+  },
+  {
+    "revision": "1f5476219313e81689d28293b43810c0",
+    "url": "/static/media/FlipCupTexture.1f547621.webm"
+  },
+  {
+    "revision": "3f33539d53d3166fe321e8f00d487322",
+    "url": "/static/media/FlipCupTexture06.3f33539d.webm"
+  },
+  {
+    "revision": "6f9a03bd4554e5454de1c79f4c91aadf",
+    "url": "/static/media/Linkedin.6f9a03bd.jpg"
+  },
+  {
+    "revision": "d000baccb58a8b22874d353b71a54844",
+    "url": "/static/media/MIRLOS.d000bacc.mp3"
+  },
+  {
+    "revision": "28f8d53f604be318c9d7088043bbac14",
+    "url": "/static/media/VideoA-3M.28f8d53f.webm"
+  },
+  {
+    "revision": "e74d260529252c3cde70b68f3fcbd54e",
+    "url": "/static/media/VideoA-Bike.e74d2605.webm"
+  },
+  {
+    "revision": "f348c0e27a3a912d18802838a68e6db3",
+    "url": "/static/media/VideoA-EsferaRental.f348c0e2.webm"
+  },
+  {
+    "revision": "ec5b4148ab2c2fdec7baa186f307d7ec",
+    "url": "/static/media/VideoA-FirstStopHealth.ec5b4148.webm"
+  },
+  {
+    "revision": "cf8fa3d5facafbea0dabc85cafd1c0ec",
+    "url": "/static/media/VideoA-Glassfrog.cf8fa3d5.webm"
+  },
+  {
+    "revision": "29714cde84781c0792b98b9dee62b38e",
+    "url": "/static/media/VideoA-GoldMetrics.29714cde.webm"
+  },
+  {
+    "revision": "a5ab9149c4a87bb67e664a654cd9b891",
+    "url": "/static/media/VideoA-Lodgify.a5ab9149.webm"
+  },
+  {
+    "revision": "fa1c1a68b67296bbd797101323dc8ced",
+    "url": "/static/media/VideoA-Number.fa1c1a68.webm"
+  },
+  {
+    "revision": "692e72fbe73b5959d272230585030181",
+    "url": "/static/media/VideoB-3M.692e72fb.webm"
+  },
+  {
+    "revision": "7a6deeb1ec4a292be2270c65579e9591",
+    "url": "/static/media/VideoB-Bike.7a6deeb1.webm"
+  },
+  {
+    "revision": "059634e69ebcc13235e3e552aceacd5f",
+    "url": "/static/media/VideoB-EsferaRental.059634e6.webm"
+  },
+  {
+    "revision": "0e997d3e1ae4f1ec757e830bc9788dac",
+    "url": "/static/media/VideoB-FirstStopHealth.0e997d3e.webm"
+  },
+  {
+    "revision": "bc8d0fd3f1ae8df9e433f0988bf2378b",
+    "url": "/static/media/VideoB-Glassfrog.bc8d0fd3.webm"
+  },
+  {
+    "revision": "7763b0be62b54155b640e230943b67aa",
+    "url": "/static/media/VideoB-GoldMetrics.7763b0be.webm"
+  },
+  {
+    "revision": "ec1b29c6afd66bd81cc34015a2c17a50",
+    "url": "/static/media/VideoB-Lodgify.ec1b29c6.webm"
+  },
+  {
+    "revision": "e90eabd6ace29764f273f760ecb82d53",
+    "url": "/static/media/VideoB-Number.e90eabd6.webm"
+  },
+  {
+    "revision": "70743674ee0839f3bfb0c9296cc406cb",
+    "url": "/static/media/VideoB-codeChallenges.70743674.webm"
+  },
+  {
+    "revision": "319fd781c06e53688eab5001cc874fd3",
+    "url": "/static/media/VideoB-ravenous.319fd781.webm"
+  },
+  {
+    "revision": "6431b5f3e327c219e77ccca3dcfefdcf",
+    "url": "/static/media/Web1.6431b5f3.png"
+  },
+  {
+    "revision": "5eb4c66f298da674d5601f321f13cd3f",
+    "url": "/static/media/Web2.5eb4c66f.JPG"
+  },
+  {
+    "revision": "5f0ece7750714b2adab93966959e350d",
+    "url": "/static/media/Web3.5f0ece77.JPG"
+  },
+  {
+    "revision": "2dc9d4bf53ade4516d586e875509d9af",
+    "url": "/static/media/david bw.2dc9d4bf.jpg"
+  },
+  {
+    "revision": "2bd76b88626c0749539106d7395618f5",
+    "url": "/static/media/github.2bd76b88.jpg"
+  },
+  {
+    "revision": "483481a62194bec75c3e5e140aa83390",
+    "url": "/static/media/videoA-Art.483481a6.webm"
+  },
+  {
+    "revision": "4d371b28daabbb1c519ae0f86b9c94f8",
+    "url": "/static/media/videoA-Countries.4d371b28.webm"
+  },
+  {
+    "revision": "a812efc5b3d5df6569c04f67dea70ba5",
+    "url": "/static/media/videoA-Dashboard.a812efc5.webm"
+  },
+  {
+    "revision": "505bb2dfa0ab7aba870274a57eaf37a9",
+    "url": "/static/media/videoA-FCC.505bb2df.webm"
+  },
+  {
+    "revision": "318dc1a0e0d9451938deda0e76aff3d1",
+    "url": "/static/media/videoA-Jammming.318dc1a0.webm"
+  },
+  {
+    "revision": "f17c26c846e42316a50728df21d3b1f3",
+    "url": "/static/media/videoA-PPT.f17c26c8.webm"
+  },
+  {
+    "revision": "9ea658160b8342a9ce14785608ebaed5",
+    "url": "/static/media/videoA-codeChallenges.9ea65816.webm"
+  },
+  {
+    "revision": "644b9c4a598c92f714029aed2c644d3f",
+    "url": "/static/media/videoA-portafolio.644b9c4a.webm"
+  },
+  {
+    "revision": "2a03516d65fb0f428991bcba16815f15",
+    "url": "/static/media/videoA-ravenous.2a03516d.webm"
+  },
+  {
+    "revision": "4cc1655eaaf63d3e8a38839b36bc00ff",
+    "url": "/static/media/videoB-Art.4cc1655e.webm"
+  },
+  {
+    "revision": "fd53a08b0830144b55235fa93554ed33",
+    "url": "/static/media/videoB-Countries.fd53a08b.webm"
+  },
+  {
+    "revision": "f97c44eee48db27887159e30b353dba3",
+    "url": "/static/media/videoB-Dashboard.f97c44ee.webm"
+  },
+  {
+    "revision": "5526a69b7b3a9f2e8a6f60cc87cf9d59",
+    "url": "/static/media/videoB-FCC.5526a69b.webm"
+  },
+  {
+    "revision": "43d4a909104f78c69d82518cc7e3a20e",
+    "url": "/static/media/videoB-Jammming.43d4a909.webm"
+  },
+  {
+    "revision": "06ce4500374f57d716ea7d2adf792d24",
+    "url": "/static/media/videoB-PPT.06ce4500.webm"
+  },
+  {
+    "revision": "89f84b79141c8a4022aeb33ee9420d00",
+    "url": "/static/media/videoB-portafolio.89f84b79.webm"
+  },
+  {
+    "revision": "54fbd1e82c0f611c3cb347dc15063bea",
+    "url": "/static/media/watercolor.54fbd1e8.webm"
+  }
+]);
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -21,11 +21,10 @@ function Home() {
         loop
         muted
         controls={false}
-        width="100%" 
-        height="100%"
-        className='Background'
-        playsinline >
-        <source src={background} type='video/mp4' />
+        playsInline
+        preload="metadata"
+        className='Background'>
+        <source src={background} type='video/webm' />
       </video>
       <div className="gridContainer">
         <div className="gridItemA">
@@ -37,7 +36,12 @@ function Home() {
         <div className="gridItemB" >
           <GlassmorphismCard />
           <p id="about-me" style={{ fontSize: '1.4rem', borderBottom: 'solid', borderTop: 'solid', paddingTop: '5%', paddingBottom: '5%', borderWidth: 'thin' }}>
-          I am a software engineer with a strong background in frontend architecture, scalable web applications, and client-facing solutions. I design and build accessible, performant user experiences using modern HTML, CSS, and JavaScript frameworks, especially React. I have hands-on experience integrating AI and LLM technologies into web applications, enabling intelligent features and enhanced user interactions. I thrive in remote and distributed teams, and I adapt quickly to the tools and workflows required by the project. I hold dual nationality and am currently based in Central Europe, though I previously worked from SJO, Costa Rica. I am fluent in Français, Español, and English. When I am not coding, I explore short-film production, painting, and yoga. I also have experience with:<span><HomeHook /></span></p>
+          I am a software engineer with a strong background in frontend architecture, scalable web applications, and client-facing solutions. I design
+          
+           and build accessible, performant user experiences using modern HTML, CSS, and JavaScript frameworks, especially React. I have hands-on experience integrating AI and LLM technologies into web applications, enabling intelligent features and enhanced user interactions. I thrive in remote and distributed teams, and I adapt quickly to the tools and workflows required by 
+           the project. I hold dual nationality and am currently based in Central Europe, though 
+           I previously worked from SJO, Costa Rica. I am fluent in Français, Español, and English. 
+           <p id="about-me" style={{ fontSize: '1.4rem', paddingTop: '5%', paddingBottom: '5%', borderWidth: 'thin' }}>You can <Link to="/portfolio" className="aboutLink">visit my portfolio</Link> to see how I bring ideas to life.</p> I have experience with:<span><HomeHook /></span></p>
         </div>
         <div className="gridItemC">
           <h1 className="contactText" style={{ gridColumnStart: '1', gridColumnEnd: '2', margin: '0px', width: '50%', justifySelf: 'center', alignSelf: 'center' }}>Interested in doing a project together?</h1>
